d96fa20ce463572ea6e777c02261bd238d9df029
Snigdha Mohapatra <snigdhamohapatra@Snigdhas-MacBook-Pro.local>
2020-02-23 06:54:44 -0500

    Updated

--- src/main/java/stepDefinitions/LoginStepDefinition.java
+++ src/main/java/stepDefinitions/LoginStepDefinition.java
@@ -1,13 +1,21 @@
 package stepDefinitions;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import cucumber.api.java.en.Given;
 
 public class LoginStepDefinition {
 	
+	WebDriver driver;
 	
 	@Given("^User is already on login page$")
 	public void user_is_already_on_login_page()  {
+		System.setProperty("webdriver.chrome.driver", "/Users/snigdhamohapatra/Jars/chromedriver");
 	    // Write code here that turns the phrase above into concrete actions
+		driver = new ChromeDriver();
+		driver.get("https://gmail.com");
+		
 	   
 	}
 
